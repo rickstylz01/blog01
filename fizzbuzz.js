45611cae984ba1917c6f33d72899bbb6cbc543a9
Ricardo Maya <maya_ricardo@yahoo.com>
2018-06-22 18:40:39 -0500

    Add files via upload
    This is the regular fizzbuzz original question w/o the for loop.

---
+++ fizzbuzz.js
@@ -0,0 +1,16 @@
+var randomNumber = Math.round(Math.random() * 15);
+
+if(randomNumber%3 === 0 && randomNumber != 0){
+	if(randomNumber%5 === 0){
+		alert("fizzbuzz");
+	}
+	else {
+		alert("buzz");
+	}
+}
+else if(randomNumber%5 === 0 && randomNumber != 0){
+	alert("buzz");
+}
+else {
+	console.log(randomNumber);
+}
